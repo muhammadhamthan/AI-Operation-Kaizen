eaa586ab87fcb9ea56fa9da0435b0684868a7fd6
Anish Kumar <akcoder1102004@gmail.com>
2026-03-10 16:28:43 +0530

    Changed the vercel.json

--- Full Stack/frontend/src/services/api.js
+++ Full Stack/frontend/src/services/api.js
@@ -29,7 +29,7 @@ import { withRetry } from '../utils/networkRetry';
 //   return 'http://localhost:8001/api';
 // };
 
-const backendUrl = 'http://127.0.0.1:8000';
+const backendUrl = 'http://13.48.25.159:8000';
 
 const API_BASE_URL = backendUrl;
 
